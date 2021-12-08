8a04d323009a1e3d04305d9da9a94e90b958c1c7
Pernilla Lindström <pernilla_lindstrom@outlook.com>
2021-12-08 23:25:22 +0100

    some css

--- js/posts.js
+++ js/posts.js
@@ -17,6 +17,7 @@ async function blogPosts() {
           <h1> ${post.title}</h1>
           <p>${post.author} ⬥ ${formatedDate}</p>         
           <p>Tags: ${post.tags.join(', ')}</p>
+          <hr>
           <p> ${post.content}  <a href="details.html?id=${post._id}">see more</a>
           </p>
           <hr>
